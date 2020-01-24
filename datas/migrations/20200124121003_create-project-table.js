6300f9d1d88f3e18e2bebf29b801f0b04e729750
Babatunde <babatundea15@gmail.com>
2020-01-24 13:03:34 +0100

    I created the tasks table

--- datas/migrations/20200124121003_create-project-table.js
+++ datas/migrations/20200124121003_create-project-table.js
@@ -1,24 +1,49 @@
 exports.up = function(knex) {
   // / create the resources table
-  return knex.schema
-    .createTable("resources", table => {
-      table.increments(); // This creates column named "id" which is the primary key.
-      table
-        .text("resource_name")
-        .unique()
-        .notNullable();
-      table.text("description");
-    })
-    // This is the projects table
-    .createTable("projects", table => {
-      table.increments();
-      table
-        .text("Name", 150)
-        .unique()
-        .notNullable();
-      table.text("description");
-      table.boolean("Completed").defaultTo(false);
-    });
+  return (
+    knex.schema
+      .createTable("resources", table => {
+        table.increments(); // This creates column named "id" which is the primary key.
+        table
+          .text("resource_name")
+          .unique()
+          .notNullable();
+        table.text("description");
+      })
+      // This is the projects table
+      .createTable("projects", table => {
+        table.increments();
+        table
+          .text("Name")
+          .unique()
+          .notNullable();
+        table.text("description");
+        table.boolean("Completed").defaultTo(false);
+      })
+      // The tasks table comes here
+      .createTable("tasks", tbl => {
+        tbl.increments();
+        tbl.text("Description").notNullable();
+        tbl.text("Notes");
+        tbl.boolean("Completed").defaultTo(false);
+        tbl
+          .integer("ProjectID")
+          .unsigned()
+          .notNullable()
+          .references("id")
+          .inTable("projects")
+          .onUpdate("CASCADE")
+          .onDelete("CASCADE");
+        tbl
+          .integer("ResourceID")
+          .unsigned()
+          .notNullable()
+          .references("id")
+          .inTable("resources")
+          .onUpdate("CASCADE")
+          .onDelete("CASCADE");
+      })
+  );
 };
 
 exports.down = function(knex) {
