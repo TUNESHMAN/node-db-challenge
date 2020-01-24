29ebc5968bab9483708e12aabb845d18fdfdc58f
Babatunde <babatundea15@gmail.com>
2020-01-24 12:59:50 +0100

    I created projects table

--- datas/migrations/20200124121003_create-project-table.js
+++ datas/migrations/20200124121003_create-project-table.js
@@ -1,16 +1,27 @@
 exports.up = function(knex) {
   // / create the resources table
-  return knex.schema.createTable('resources', table => {
-    table.increments(); // This creates column named "id" which is the primary key.
-    table
-      .text("resource_name")
-      .unique()
-      .notNullable();
-    table.text("description");
-  });
+  return knex.schema
+    .createTable("resources", table => {
+      table.increments(); // This creates column named "id" which is the primary key.
+      table
+        .text("resource_name")
+        .unique()
+        .notNullable();
+      table.text("description");
+    })
+    // This is the projects table
+    .createTable("projects", table => {
+      table.increments();
+      table
+        .text("Name", 150)
+        .unique()
+        .notNullable();
+      table.text("description");
+      table.boolean("Completed").defaultTo(false);
+    });
 };
 
 exports.down = function(knex) {
   // Drop the resources table if it exists
-  return knex.schema.dropTableIfExist('resources');
+  return knex.schema.dropTableIfExist("resources");
 };
